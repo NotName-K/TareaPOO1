82c2fef47794142a7c4966411870273315f6e610
Kevin <kecastellanosp@unal.edu.co>
2025-04-25 21:49:21 -0500

    joao

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author Goku
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        int a = 3;
+        System.out.println(a);
+    }
+}
