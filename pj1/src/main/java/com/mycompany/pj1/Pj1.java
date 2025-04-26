79446274b98c66993d0704d7921993d65be6532c
Kevin <kecastellanosp@unal.edu.co>
2025-04-26 12:27:13 -0500

    user2

--- pj1/src/main/java/com/mycompany/pj1/Pj1.java
+++ pj1/src/main/java/com/mycompany/pj1/Pj1.java
@@ -6,7 +6,7 @@ public class Pj1 {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Ingresa un Valor");
         int y = scanner.nextInt();
-        int z = 42;
+        int z = 425;
         int x = 10;
         float k = z * x;
         System.out.println(y + k);
