764c78064ca2caa7e4879b6960f6159556b78e86
Kevin <kecastellanosp@unal.edu.co>
2025-04-26 12:23:44 -0500

    user

--- pj1/src/main/java/com/mycompany/pj1/Pj1.java
+++ pj1/src/main/java/com/mycompany/pj1/Pj1.java
@@ -1,12 +1,15 @@
 package com.mycompany.pj1;
-	
+import java.util.Scanner;	
 
 public class Pj1 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingresa un Valor");
+        int y = scanner.nextInt();
         int z = 42;
         int x = 10;
         float k = z * x;
-        System.out.println(k);
+        System.out.println(y + k);
         
     }
 }
