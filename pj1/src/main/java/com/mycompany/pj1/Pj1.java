b6adf7d5ccb1560b2039496667347a8781ece277
Kevin <kecastellanosp@unal.edu.co>
2025-04-26 12:02:32 -0500

    suma

---
+++ pj1/src/main/java/com/mycompany/pj1/Pj1.java
@@ -0,0 +1,11 @@
+package com.mycompany.pj1;
+	
+
+public class Pj1 {
+    public static void main(String[] args) {
+        int z = 42;
+        int x = 10;
+        float k = x + z;
+        System.out.println(k);
+    }
+}
