001b55dd20cafce0e4dd92fb0ad48ec9724beaa4
Kevin <kecastellanosp@unal.edu.co>
2025-04-26 12:46:23 -0500

    user3

--- pj1/src/main/java/com/mycompany/pj1/Pj1.java
+++ pj1/src/main/java/com/mycompany/pj1/Pj1.java
@@ -4,10 +4,10 @@ import java.util.Scanner;
 public class Pj1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Ingresa un Valor");
+        System.out.print("Ingresa un Valor: ");
         int y = scanner.nextInt();
         int z = 425;
-        int x = 10;
+        byte x = 10;
         float k = z * x;
         System.out.println(y + k);
         
